857abf69fcc438dabec6828cce6ee7deac45b172
Dustin Davis and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-03-23 15:36:07 -0700

    add initial keg tracker files

---
+++ app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from 'angular2/core';
+
+@Component({
+  selector: 'my-app',
+  template: `
+    <div class="container">
+      <h1>Keg Tracker</h1>
+    </div>
+  `
+})
+
+export class AppComponent {
+
+}
