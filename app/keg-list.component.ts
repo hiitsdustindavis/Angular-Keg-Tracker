c5b657507e39e969e301c90e83b762651f6ac978
Dustin Davis and Michelle Brecunier <hiitsdustindavis@gmail.com>
2016-03-24 13:22:51 -0700

    add the sellpint and edit keg components

--- app/keg-list.component.ts
+++ app/keg-list.component.ts
@@ -1,33 +1,24 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { NewKegComponent } from './new-keg.component';
 import { KegDisplayComponent } from './keg-display.component';
-import { EditKegComponent } from './edit-keg.component';
 import { Keg } from './keg.model';
 
 @Component ({
   selector: 'keg-list',
   inputs: ['kegList'],
-  outputs: ['onKegSelect'],
-  directives: [KegDisplayComponent, NewKegComponent, EditKegComponent],
+  directives: [KegDisplayComponent, NewKegComponent],
   template: `
 
     <keg-display *ngFor="#keg of kegList"
-    (click)="kegClicked(keg)"
-    [class.selected]="keg === selectedKeg"
     [keg]="keg">
     </keg-display>
-    <edit-keg *ngIf="selectedKeg" [keg]="selectedKeg"></edit-keg>
     <new-keg (onSubmitNewKeg)="createKeg($event)"></new-keg>
   `
 })
 
 export class KegListComponent {
   public kegList: Keg[];
-  public onKegSelect: EventEmitter<Keg>;
-  public selectedKeg: Keg;
-  constructor() {
-    this.onKegSelect = new EventEmitter();
-  }
+
   createKeg(newKeg: string): void {
     var kegPrice: number = parseFloat(newKeg[2]);
     var kegAbv: number = parseFloat(newKeg[3]);
@@ -35,9 +26,6 @@ export class KegListComponent {
       new Keg(newKeg[0], newKeg[1], kegPrice, kegAbv, this.kegList.length)
     )
   }
-  kegClicked(clickedKeg: Keg): void {
-    this.selectedKeg = clickedKeg;
-    this.onKegSelect.emit(clickedKeg);
-  }
+
 
 }
--- app/pour-pint.component.ts
+++ app/pour-pint.component.ts
@@ -1,2 +1,19 @@
 import { Component } from 'angular2/core';
 import { Keg } from './keg.model';
+
+@Component ({
+  selector: 'pour-pint',
+  inputs: ['keg'],
+  template: `
+    <div>
+      <button (click)="pintPoured()">Sell Pint!</button>
+    </div>
+  `
+})
+
+export class PourPintComponent {
+  public keg: Keg;
+  pintPoured(){
+    this.keg.pintsLeft -=1;
+  }
+}
